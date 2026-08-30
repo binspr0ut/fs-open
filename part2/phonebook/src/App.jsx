5abe2ced68c21881f08200388ad1d2f365cf396e
binspr0ut <plattenecho@gmail.com>
2026-08-30 23:48:57 +0700

    add phonebook exercise

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas2' }
+
+  ])
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => (<div key={Math.random()}>{person.name}</div>))}
+      </div>
+    </div>
+  )
+}
+
+export default App
